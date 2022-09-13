ca6a457fbabdf950a3c5a228f1c8a425d176ea7a
heiyu <304429477@qq.com>
2022-09-13 18:27:03 +0800

    项目初始化+生成逆向文件

---
+++ heiyu-mall/src/main/java/com/heiyu/mall/model/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package com.heiyu.mall.model.dao;
+
+import com.heiyu.mall.model.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
---
+++ heiyu-mall/src/main/java/com/heiyu/mall/model/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.heiyu.mall.model.dao;
+
+import com.heiyu.mall.model.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
