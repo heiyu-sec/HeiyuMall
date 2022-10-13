634eabfbe74dc47b00f18b4a7ca53e83a8e0e18f
heiyu <304429477@qq.com>
2022-10-13 23:42:39 +0800

    增加优雅的vali注解

--- heiyu-mall/src/main/java/com/heiyu/mall/exctption/GlobalExceptionHandler.java
+++ heiyu-mall/src/main/java/com/heiyu/mall/exctption/GlobalExceptionHandler.java
@@ -5,9 +5,15 @@ import com.heiyu.mall.common.ApiRestResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 描述：     处理统一异常的handler
@@ -29,5 +35,31 @@ public class GlobalExceptionHandler {
     public Object handleImoocMallException(ImoocMallException e) {
         log.error("ImoocMallException: ", e);
         return ApiRestResponse.error(e.getCode(), e.getMessage());
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public ApiRestResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        log.error("MethodArgumentNotValidException:",e);
+        return handleBingdingResult(e.getBindingResult());
+    }
+
+    private ApiRestResponse handleBingdingResult(BindingResult result){
+            //把异常处理为对外暴露的提示
+        List<String> list = new ArrayList<>();
+        if (result.hasErrors()){
+            List<ObjectError> allErrors=result.getAllErrors();
+            for (int i = 0; i < allErrors.size(); i++) {
+                ObjectError objectError =  allErrors.get(i);
+                String message=objectError.getDefaultMessage();
+                list.add(message);
+            }
+
+        }
+
+        if(list.size()==0){
+            return ApiRestResponse.error(ImoocMallExceptionEnum.REQUEST_PARAM_ERROR);
+        }
+        return ApiRestResponse.error(ImoocMallExceptionEnum.REQUEST_PARAM_ERROR.getCode(),list.toString());
+
     }
 }
--- heiyu-mall/src/main/java/com/heiyu/mall/model/request/AddCategoryReq.java
+++ heiyu-mall/src/main/java/com/heiyu/mall/model/request/AddCategoryReq.java
@@ -1,12 +1,22 @@
 package com.heiyu.mall.model.request;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 /**
  * 描述：添加目录的请求类
  */
 public class AddCategoryReq {
+    @Size(min = 2,max = 5)
+    @NotNull
     private String name;
+    @NotNull
+    @Max(3)
     private Integer type;
+    @NotNull
     private Integer parentId;
+    @NotNull
     private Integer orderNum;
 
     public String getName() {
