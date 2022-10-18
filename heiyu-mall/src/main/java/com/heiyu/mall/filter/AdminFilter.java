212d12408067059a7699491f8491136db3b55082
heiyu <304429477@qq.com>
2022-10-18 11:04:43 +0800

    admin类update1

---
+++ heiyu-mall/src/main/java/com/heiyu/mall/filter/AdminFilter.java
@@ -0,0 +1,25 @@
+package com.heiyu.mall.filter;
+
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * 描述： 管理员校验过滤器
+ */
+public class AdminFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+}
