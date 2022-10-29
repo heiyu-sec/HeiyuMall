3de4eafdd836c03adacbe7f3e636de6a0ca5232c
heiyu <304429477@qq.com>
2022-10-29 22:53:48 +0800

    初始化UUID

--- heiyu-mall/src/main/java/com/heiyu/mall/controller/ProductAdminController.java
+++ heiyu-mall/src/main/java/com/heiyu/mall/controller/ProductAdminController.java
@@ -7,9 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.util.UUID;
 
 /**
  * 描述：后台商铺管理Controller
@@ -23,5 +27,16 @@ public class ProductAdminController {
     public ApiRestResponse addProduct(@Valid @RequestBody AddProductReq addProductReq){
         productService.add(addProductReq);
         return ApiRestResponse.success();
+    }
+    @PostMapping("admin/upload/file")
+    public ApiRestResponse upload(HttpServletRequest httpServletRequest, @RequestParam("file") MultipartFile file){
+        String fileName = file.getOriginalFilename();
+        String suffixName = fileName.substring(fileName.lastIndexOf("."));
+        //生成文件名称uuid
+        UUID uuid = UUID.randomUUID();
+        String newFileName = uuid.toString() + suffixName;
+
+
+
     }
 }
--- heiyu-mall/src/main/java/com/heiyu/mall/model/vo/CategoryVO.java
+++ heiyu-mall/src/main/java/com/heiyu/mall/model/vo/CategoryVO.java
@@ -87,4 +87,6 @@ public class CategoryVO implements Serializable {
     public void setUpdateTime(Date updateTime) {
         this.updateTime = updateTime;
     }
+
+    
 }
