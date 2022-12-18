8ac3cbfc57363ca284109e395917402830cb715a
heiyu <304429477@qq.com>
2022-12-18 22:17:29 +0800

    添加数据库事务

--- heiyu-mall/src/main/java/com/heiyu/mall/service/impl/OrderServiceImpl.java
+++ heiyu-mall/src/main/java/com/heiyu/mall/service/impl/OrderServiceImpl.java
@@ -18,6 +18,7 @@ import com.heiyu.mall.service.OrderService;
 import com.heiyu.mall.util.OrderCodeFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 
 import java.util.ArrayList;
@@ -46,6 +47,8 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     OrderItemMapper orderItemMapper;
 
+    //数据库事务
+    @Transactional(rollbackFor = Exception.class)
     @Override
     public String create(CreateOrderReq createOrderReq){
         //拿到用户ID
