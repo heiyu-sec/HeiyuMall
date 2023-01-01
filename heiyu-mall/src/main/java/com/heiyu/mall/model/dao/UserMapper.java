b7931bd88a410ee36d1c64c2ed4c6019292316e5
heiyu <304429477@qq.com>
2023-01-01 22:16:41 +0800

    邮箱2

--- heiyu-mall/src/main/java/com/heiyu/mall/model/dao/UserMapper.java
+++ heiyu-mall/src/main/java/com/heiyu/mall/model/dao/UserMapper.java
@@ -1,10 +1,7 @@
 package com.heiyu.mall.model.dao;
 
 import com.heiyu.mall.model.pojo.User;
-import org.apache.ibatis.annotations.Param;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserMapper {
     int deleteByPrimaryKey(Integer id);
 
@@ -17,8 +14,4 @@ public interface UserMapper {
     int updateByPrimaryKeySelective(User record);
 
     int updateByPrimaryKey(User record);
-
-    User selectByName(String userName);
-
-    User selectLogin(@Param("userName") String userName, @Param("password")String password);
 }
